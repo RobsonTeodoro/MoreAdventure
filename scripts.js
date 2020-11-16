fb7bb56efc4293bbd2caca39eba875bf37d17b62
trilhandoteresopolis <62854728+RobsonTeodoro@users.noreply.github.com>
2020-11-16 03:56:26 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,16 +1,30 @@
-function onOff(){
-  document
-  .querySelector("#modal")
-  .classList
-  .toggle("hide")
+function carregar() {
+  var msg = window.document.getElementById("msg");
+  var img = window.document.getElementById("imagem");
 
-  document
-  .querySelector("body")
-  .classList
-  .toggle("hideScroll")
+  var dataHora = new Date();
+  var hora = dataHora.getHours();
 
-  document
-  .querySelector("#modal")
-  .classList
-  .toggle("addScroll")
+  var dataMinuto = new Date() ;
+  var minuto = dataMinuto.getMinutes()
+
+  //var hora = 22;
+  //var minuto = 44;
+
+  if (hora >= 0 && hora < 12) {
+    //bom dia
+    msg.innerHTML = `<rtrs style="color: green;"> <strong>Bom dia! São ${hora} hora(s) e ${minuto} minutos! </strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
+    img.src = "Imagens/Produtos/produtos2.jpg";
+    
+  } else if (hora >= 12 && hora < 18) {
+    //boa tarde
+    msg.innerHTML = `<rtrs style="color: green;"> <strong>Boa tarde! São ${hora} hora(s) e ${minuto} minutos!</strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
+    img.src = "Imagens/Produtos/caneca.jpg";
+    
+  } else {
+    //boa noite
+    msg.innerHTML = `<rtrs style="color: green;"> <strong>Boa noite! São ${hora} hora(s) e ${minuto} minutos!</strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
+    img.src = "Imagens/Produtos/agenda.jpg";
+    
+  }
 }
