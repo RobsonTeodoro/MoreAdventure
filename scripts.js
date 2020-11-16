44b9d94b089a34abfdde3a7497c5a36a6c7fe36e
trilhandoteresopolis <62854728+RobsonTeodoro@users.noreply.github.com>
2020-11-16 04:13:06 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -13,17 +13,17 @@ function carregar() {
 
   if (hora >= 0 && hora < 12) {
     //bom dia
-    msg.innerHTML = `<rtrs style="color: green;"> <strong>Bom dia! São ${hora} hora(s) e ${minuto} minutos! </strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
+    msg.innerHTML = `<rtrs style="color: green;"> <strong>Bom dia! São ${hora} hora(s) e ${minuto} minuto(s)! </strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
     img.src = "Imagens/Produtos/produtos2.jpg";
     
   } else if (hora >= 12 && hora < 18) {
     //boa tarde
-    msg.innerHTML = `<rtrs style="color: green;"> <strong>Boa tarde! São ${hora} hora(s) e ${minuto} minutos!</strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
+    msg.innerHTML = `<rtrs style="color: green;"> <strong>Boa tarde! São ${hora} hora(s) e ${minuto} minuto(s)!</strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
     img.src = "Imagens/Produtos/caneca.jpg";
     
   } else {
     //boa noite
-    msg.innerHTML = `<rtrs style="color: green;"> <strong>Boa noite! São ${hora} hora(s) e ${minuto} minutos!</strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
+    msg.innerHTML = `<rtrs style="color: green;"> <strong>Boa noite! São ${hora} hora(s) e ${minuto} minuto(s)!</strong></rtrs> <br> Clique <a href="#enviar">aqui</a> e venha trilhar com a gente!`
     img.src = "Imagens/Produtos/agenda.jpg";
     
   }
